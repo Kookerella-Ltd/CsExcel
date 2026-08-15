dddcc03944b26d7a474ad6ccdef689c9907ffcd0
mrnicholls <m_r_nicholls@hotmail.com>
2026-08-15 11:11:58 +0100

    ss

--- CsExcel/Fluent.fs
+++ CsExcel/Fluent.fs
@@ -1,13 +1,14 @@
-﻿namespace CsExcel.Fluent
+namespace CsExcel.Fluent
 
 open System.Runtime.CompilerServices
 open FsExcel
 open System.Linq
 open System
 open CsExcel
+open ClosedXML.Excel
 
 [<Extension>]
-type CellPropSeqExtensions = 
+type CellPropSeqExtensions =
     [<Extension>]
 // need to work out the efficiency here
     static member AddString(this : CellProp seq, s : string) = this.Prepend(CellProp.String s)
@@ -57,92 +58,52 @@ type CellPropSeqExtensions =
     static member AddCellSize(this : CellProp seq, size) = CellProp.CellSize size |> this.Prepend
 
 [<Extension>]
-type RenderExtensions = 
+type RenderExtensions =
     [<Extension>]
-    static member AsFile(cells : Item seq,path : string) = 
+    static member AsFile(cells : Item seq,path : string) =
         Render.AsFile(cells,path)
-
-
-//    [<Extension>]
-//    static member AddFloat(this : CellProp list, f : float) =
-//        CellProp.Float f :: this
-
-//    [<Extension>]
-//    static member AddInteger(this : CellProp list, i : int) =
-//        CellProp.Integer i :: this
-
-//    [<Extension>]
-//    static member AddBoolean(this : CellProp list, b : bool) =
-//        CellProp.Boolean b :: this
-
-//    [<Extension>]
-//    static member AddDateTime(this : CellProp list, dt : DateTime) =
-//        CellProp.DateTime dt :: this
-
-//    [<Extension>]
-//    static member AddTimeSpan(this : CellProp list, ts) =
-//        CellProp.TimeSpan ts :: this
-//module CellPropFactory = 
-//    let String(s : string) = CellProp.String s
-//    let Float(f : float) = CellProp.Float f
-//    let Integer(i : int) = CellProp.Integer i
-//    let Boolean(b : bool) = CellProp.Boolean b
-//    let DateTime(dt : DateTime) = CellProp.DateTime dt
-//    let TimeSpan(ts) = CellProp.TimeSpan ts
-//    let FormulaA1(formula : string) = CellProp.FormulaA1 formula
-//    let Next(pos) = CellProp.Next pos
-//    let FontEmphasis(emp) = CellProp.FontEmphasis emp
-//    let FontName(name) = CellProp.FontName name
-//    let FontSize(size) = CellProp.FontSize size
-//    let FontColor(color) = CellProp.FontColor color
-//    let Border(border) = CellProp.Border border
-//    let BorderColor(color) = CellProp.BorderColor color
-//    let BackgroundColor(color) = CellProp.BackgroundColor color
-//    let HorizontalAlignment(alignment) = CellProp.HorizontalAlignment alignment
-//    let VerticalAlignment(alignment) = CellProp.VerticalAlignment alignment
-//    let TextRotation(rotation) = CellProp.TextRotation rotation
-//    let WrapText(wrap) = CellProp.WrapText wrap
-//    let FormatCode(formatCode) = CellProp.FormatCode formatCode
-//    let Name(name) = CellProp.Name name
-//    let ScopedName(name) = CellProp.ScopedName name
-//    let CellSize(size) = CellProp.CellSize size
-
+    [<Extension>]
+    static member AsHtml(cells : Item seq, isHeader : Render.IsHeader) =
+        Render.AsHtml(cells, isHeader)
+    [<Extension>]
+    static member AsStream(cells : Item seq, stream) =
+        Render.AsStream(cells, stream)
+    [<Extension>]
+    static member AsStreamBytes(cells : Item seq) =
+        Render.AsStreamBytes(cells)
+    [<Extension>]
+    static member AsWorkBook(cells : Item seq) =
+        Render.AsWorkBook(cells)
 
 module ItemFactory =
     let Cell(fprops : Func<CellProp seq,CellProp seq>) =
         fprops.Invoke [||]
         |> ItemFactory.Cell
-    //let Style(props : CellProp seq) =
-    //    props
-    //    |> Seq.toList
-    //    |> Item.Style
-    //let AutoFilter(filters : AutoFilter seq) =
-    //    filters
-    //    |> Seq.toList
-    //    |> Item.AutoFilter
-    //let BorderMergedCell(borderProps : StyleMergedCell seq) =
-    //    borderProps
-    //    |> Seq.toList
-    //    |> Item.BorderMergedCell
-    let Go(position) = 
+    let Style(fprops : Func<CellProp seq,CellProp seq>) =
+        fprops.Invoke [||]
+        |> ItemFactory.Style
+    let AutoFilter(filters : AutoFilter seq) =
+        filters
+        |> ItemFactory.AutoFilter
+    let BorderMergedCell(borderProps : StyleMergedCell seq) =
+        borderProps
+        |> ItemFactory.BorderMergedCell
+    let Go(position) =
         Item.Go position
-    //let Worksheet(name) = 
-    //    Item.Worksheet name
-    //let AutoFit(autoFit) = 
-    //    Item.AutoFit autoFit
-    //let Workbook(xlWorkbook) = 
-    //    Item.Workbook xlWorkbook
-    //let InsertRowsAbove(rows) = 
-    //    Item.InsertRowsAbove rows
-    //let SizeAll(size) = 
-    //    Item.SizeAll size
-    //let MergeCells(c1,c2) =
-    //    Item.MergeCells(c1,c2)
-    //let Table(props : TableProperty seq) =
-    //    props
-    //    |> Seq.toList
-    //    |> Item.Table
-    //let FreezePanes(freezePanes) = 
-    //    Item.FreezePanes freezePanes
-
-
+    let Worksheet(name) =
+        Item.Worksheet name
+    let AutoFit(autoFit) =
+        Item.AutoFit autoFit
+    let Workbook(xlWorkbook : XLWorkbook) =
+        Item.Workbook xlWorkbook
+    let InsertRowsAbove(rows) =
+        Item.InsertRowsAbove rows
+    let SizeAll(size) =
+        Item.SizeAll size
+    let MergeCells(c1,c2) =
+        Item.MergeCells(c1,c2)
+    let Table(props : TableProperty seq) =
+        props
+        |> ItemFactory.Table
+    let FreezePanes(freezePanes) =
+        Item.FreezePanes freezePanes
